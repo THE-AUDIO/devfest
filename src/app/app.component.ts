dbf9e34044a85d3f25a121cb05845ef940e58241
THE-AUDIO <nandraina.dev22@gmail.com>
2024-11-14 21:10:23 +0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as THREE from "three";
+import { WINDOW } from './window.token';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'devFest';
+  constructor(@Inject(WINDOW) private window: Window) {}
+  ngAfterViewInit(): void {
+    const width = window.innerWidth, height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true } );
+renderer.setSize( width, height );
+renderer.setAnimationLoop( animate );
+const container = document.getElementById('3d');
+if (container) {
+  container.appendChild(renderer.domElement);
+}
+
+// animation
+
+function animate( time:number ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
+  }
 }
