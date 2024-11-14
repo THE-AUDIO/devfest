2d4249c88bc8737d7ce94635dc7fc4fec360aec4
THE-AUDIO <nandraina.dev22@gmail.com>
2024-11-15 00:19:59 +0300

    add globe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Experience } from './experience/experience.component';
-import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, Inject } from '@angular/core';
+import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { NgxBorderBeamComponent } from '@omnedia/ngx-border-beam';
+import * as THREE from 'three';
+import { WINDOW } from './window-token';
+import { NgxThreeGlobeComponent } from '@omnedia/ngx-three-globe';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: `./app.component.html`,
-    styleUrl: './app.component.scss',
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  selector: 'app-root',
+  standalone: true,
+  imports: [ NgxThreeGlobeComponent, NgxBorderBeamComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {
-    sceneGraph = Experience;
-    title = 'devFest';
+export class AppComponent implements AfterViewInit {
+  title = 'devFest';
+  isBrowser!: boolean;
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {this.isBrowser = isPlatformBrowser(this.platformId);}
+
+  ngAfterViewInit(): void {
+    if (!this.isBrowser) {
+      // Arrêtez l'exécution si `window` n'est pas défini (exécution côté serveur)
+      return;
+    }
+}
 }
